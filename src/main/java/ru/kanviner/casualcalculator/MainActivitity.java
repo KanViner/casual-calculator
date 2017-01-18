c9be8c925ab85a89f06fb217d9ecca231b17f7c5
KanViner <kanviner@gmail.com>
2017-01-18 17:19:20 +0600

    Создал проект

---
+++ src/main/java/ru/kanviner/casualcalculator/MainActivitity.java
@@ -0,0 +1,13 @@
+package ru.kanviner.casualcalculator;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivitity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_activitity);
+    }
+}
